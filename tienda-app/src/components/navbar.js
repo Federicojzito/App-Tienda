dee12a21422685fbbf4e13ef7e546d5a96b29d02
Federico Zito <federicojzito@gmail.com>
2021-03-10 20:49:04 -0300

    Agragando el NavBar, y Modificando App.js

---
+++ tienda-app/src/components/navbar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export const NavBar = () =>{
+    return (<div>
+        <h1>Tienda de Federico</h1>
+        <ul>
+            <li>
+                <a href="">Televisores</a>
+            </li>
+            <li>
+                <a href="">Computadoras</a>
+            </li>
+            <li>
+                <a href="">Accesorios</a>
+            </li>
+        </ul>
+    </div>
+    );
+};
